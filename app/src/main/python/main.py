6131d493d363ba27fdf551dacad39f95c8f4f7bc
Alejandro Lopez <aleespazx@gmail.com>
2024-01-03 01:23:46 -0600

    fix to the dark_mode logic

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -2,10 +2,10 @@ import random
 import importlib
 
 def generate(dark_mode):
-    if not dark_mode:
-        scripts = ["script_1"]
-    else:
+    if dark_mode:
         scripts = ["script_2"]
+    else:
+        scripts = ["script_1"]
     selected_script_name = random.choice(scripts)
 
     selected_script = importlib.import_module(selected_script_name)
