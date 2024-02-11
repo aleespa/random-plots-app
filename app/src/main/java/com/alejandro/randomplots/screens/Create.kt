08abe3566648bd7287363baf6b7db0344bc0cb5b
Alejandro López <aleespazx@gmail.com>
2024-02-11 19:36:07 +0000

    removla of dependencies

--- app/src/main/java/com/alejandro/randomplots/screens/Create.kt
+++ app/src/main/java/com/alejandro/randomplots/screens/Create.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,7 +29,6 @@ import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
