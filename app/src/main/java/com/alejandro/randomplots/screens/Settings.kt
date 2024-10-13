c12e4aef354249c9e482c363e0ec7e19aa0902f7
Alejandro López <aleespazx@gmail.com>
2024-10-13 17:52:10 +0100

    update dependencies

--- app/src/main/java/com/alejandro/randomplots/screens/Settings.kt
+++ app/src/main/java/com/alejandro/randomplots/screens/Settings.kt
@@ -1,9 +1,28 @@
 package com.company.test.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Settings() {
 
-    Text(text = "This are the settings")
+    Column {
+        ListItem(
+            headlineContent = {
+                Text("One line list item with 24x24 icon")
+            },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                )
+            }
+        )
+        HorizontalDivider()
+    }
 }
