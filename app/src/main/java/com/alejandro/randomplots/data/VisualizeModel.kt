2dc7c51cd1c3f360deb3a48a8ce62c52a26cb61e
Alejandro <aleespazx@gmail.com>
2024-11-16 15:17:28 +0000

    VisualizeModel

---
+++ app/src/main/java/com/alejandro/randomplots/data/VisualizeModel.kt
@@ -0,0 +1,14 @@
+package com.alejandro.randomplots.data
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.lifecycle.ViewModel
+
+class VisualizeModel: ViewModel() {
+    var loading by mutableStateOf(false)
+    var isRotated by mutableStateOf(false)
+    var imageBitmapState by mutableStateOf<ImageBitmap?>(null)
+    var latexString by mutableStateOf("")
+}
