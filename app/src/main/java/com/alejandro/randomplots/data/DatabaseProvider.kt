e204c3d053c79397890b0fca71c358a4ab4c26df
Alejandro <aleespazx@gmail.com>
2024-11-23 16:45:56 +0000

    dependencies fix

--- app/src/main/java/com/alejandro/randomplots/data/DatabaseProvider.kt
+++ app/src/main/java/com/alejandro/randomplots/data/DatabaseProvider.kt
@@ -1,18 +1,18 @@
 package com.alejandro.randomplots.data
-//import android.content.Context
-//import androidx.room.Room
-//
-//object DatabaseProvider {
-//    private var instance: AppDatabase? = null
-//
-//    fun getDatabase(context: Context): AppDatabase {
-//        if (instance == null) {
-//            instance = Room.databaseBuilder(
-//                context.applicationContext,
-//                AppDatabase::class.java,
-//                "images_database"
-//            ).build()
-//        }
-//        return instance!!
-//    }
-//}
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+    private var instance: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "images_database"
+            ).build()
+        }
+        return instance!!
+    }
+}
