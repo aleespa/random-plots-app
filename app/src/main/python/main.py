52047a6ddf1e3238161825c3515acd8fe9597638
Alejandro <aleespazx@gmail.com>
2024-11-25 22:44:07 +0000

    Select bg color

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -2,7 +2,7 @@ import os
 import importlib
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-def generate(dark_mode, script):
+def generate(dark_mode, color, script):
     selected_script = importlib.import_module(f"figures.{script}")
-    image = selected_script.create_image(dark_mode)
+    image = selected_script.create_image(dark_mode, color)
     return image
