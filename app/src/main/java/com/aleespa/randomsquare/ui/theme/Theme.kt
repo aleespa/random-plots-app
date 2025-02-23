55a493eaaf0db625e0bc11200aac2bb0422028fb
Alejandro <aleespazx@gmail.com>
2025-02-23 18:14:13 +0000

    proper colors for sdk 29

--- app/src/main/java/com/aleespa/randomsquare/ui/theme/Theme.kt
+++ app/src/main/java/com/aleespa/randomsquare/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -17,15 +18,23 @@ import androidx.core.view.WindowCompat
 import com.aleespa.randomsquare.data.SettingDarkMode
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(132, 158, 190, 255),
+    secondary = Color(91, 120, 157, 255),
+    tertiary = Color(38, 61, 91, 255),
+    background = Color(0, 6, 15),
+    surface = Color(0, 13, 30),
+    primaryContainer = Color(0, 13, 30),
+    surfaceContainer = Color(0, 13, 30),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(40, 40, 40, 255),
+    secondary = Color(143, 141, 141, 255),
+    tertiary = Color(124, 110, 110, 255),
+    background = Color(254, 252, 242),
+    surface = Color(246, 242, 233),
+    primaryContainer = Color(244, 240, 231),
+    surfaceContainer = Color(244, 240, 231),
 )
 
 @Composable
