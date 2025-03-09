10ed5ac3b507b6f936a39010120e08d9b921b4cc
Alejandro <aleespazx@gmail.com>
2025-03-09 08:47:39 +0000

    improved color buttons

--- app/src/main/java/com/aleespa/randomsquare/data/Settings.kt
+++ app/src/main/java/com/aleespa/randomsquare/data/Settings.kt
@@ -1,6 +1,9 @@
 package com.aleespa.randomsquare.data
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import kotlin.math.min
+import kotlin.math.max
 
 enum class SettingDarkMode(var text: String) {
     Auto("Auto"),
@@ -11,12 +14,12 @@ enum class SettingDarkMode(var text: String) {
 enum class BackgroundColors(var color: Color, var type: String) {
     // Dark Colors
     BLACK(Color(0, 0, 0), "Dark"),
-    RED_DARK(Color(30, 13, 0), "Dark"),
+    RED_DARK(Color(32, 15, 0), "Dark"),
     GREEN_DARK(Color(0, 30, 13), "Dark"),
     BLUE_DARK(Color(0, 13, 30), "Dark"),
-    PURPLE_DARK(Color(20, 0, 30), "Dark"),
+    PURPLE_DARK(Color(22, 0, 31), "Dark"),
     BROWN_DARK(Color(30, 15, 10), "Dark"),
-    GRAY_DARK(Color(20, 20, 20), "Dark"),
+    GRAY_DARK(Color(22, 22, 22), "Dark"),
 
     // Light Colors
     WHITE(Color(255, 255, 255), "Light"),
@@ -36,4 +39,20 @@ fun getBackgroundColorsByType(type: String): List<BackgroundColors> {
 
 fun getBackgroundColorByColor(color: Color): BackgroundColors {
     return BackgroundColors.entries.find { it.color == color } ?: BackgroundColors.BLACK
+}
+
+fun Color.adjustColor(brightnessFactor: Float = 0f, saturationFactor: Float = 0f): Color {
+    val hsv = FloatArray(3)
+    android.graphics.Color.colorToHSV(toArgb(), hsv)
+
+    // Adjust brightness
+    if (brightnessFactor > 0) {
+        hsv[2] = min(hsv[2] + brightnessFactor, 1f) // Increase brightness
+    } else if (brightnessFactor < 0) {
+        hsv[2] = max(hsv[2] + brightnessFactor, 0f) // Decrease brightness
+    }
+
+    hsv[1] = min(max(hsv[1] + saturationFactor, 0f), 1f) // Ensure within bounds
+
+    return Color(android.graphics.Color.HSVToColor(hsv))
 }
