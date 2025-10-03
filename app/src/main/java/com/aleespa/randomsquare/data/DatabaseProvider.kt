c236b0141d8e30bd50b6c7dd978061b157280f38
Alejandro <aleespazx@gmail.com>
2025-10-03 20:39:54 +0100

    Remove migration

--- app/src/main/java/com/aleespa/randomsquare/data/DatabaseProvider.kt
+++ app/src/main/java/com/aleespa/randomsquare/data/DatabaseProvider.kt
@@ -1,5 +1,4 @@
 package com.aleespa.randomsquare.data
-import MIGRATION_1_2
 import android.content.Context
 import androidx.room.Room
 
@@ -12,7 +11,6 @@ object DatabaseProvider {
                 context.applicationContext,
                 AppDatabase::class.java,
                 "images_database")
-                .addMigrations(MIGRATION_1_2)
                 .build()
         }
         return instance!!
